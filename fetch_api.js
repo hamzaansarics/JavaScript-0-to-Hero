5b1e629bd7cdd492b530adb683d7d0867d821469
Hamza Ansari <hamzaansarics@gmail.com>
2023-12-10 22:56:02 +0500

    data posting to the server with FETCH

--- fetch_api.js
+++ fetch_api.js
@@ -6,3 +6,18 @@ api.then((response)=>{
 }).then((response)=>{
     console.log( response)
 })
+
+//posting data on fake server
+fetch('https://jsonplaceholder.typicode.com/posts', {
+  method: 'POST',
+  body: JSON.stringify({
+    title: 'foo',
+    body: 'bar',
+    userId: 1,
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
